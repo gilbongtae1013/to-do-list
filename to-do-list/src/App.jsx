3dcfa1e5488c98c9cb32ea02f2766c2df8296a3a
gilbongtae1013 <gilbongtae@gmail.com>
2026-07-07 15:17:48 +0900

    규칙 추가 화면까지만 추가

--- to-do-list/src/App.jsx
+++ to-do-list/src/App.jsx
@@ -9,6 +9,12 @@ import rightarrow from './assets/arrow-right.png'
 
 function App() {
 
+    const [box_add_visible, setBox_add_visible] = useState(false);
+
+    function chooga_click() {
+        setBox_add_visible(true);
+    }
+
     function Container(props) {
     
         return (
@@ -16,7 +22,7 @@ function App() {
             <div id="box" className={props.status}>
                 <div id='box_head'>
                     <span id="status_name">{props.name}</span>
-                    <span id='chooga'>+추가</span>
+                    <span id='chooga' onClick={chooga_click}>+추가</span>
                 </div>
                 <div id='box_content'>
 
@@ -40,6 +46,29 @@ function App() {
             <Container status="daegi" name="대기¢"/>
             <Container status="jinhaeng" name="진행†"/>
             <Container status="wanryo" name="완료♤"/>
+
+            {box_add_visible && (
+                <div id="todo_box_add">
+                    <div>
+                        <label for="title_input">제목:</label>
+                        <input id="title_input"></input>
+                    </div>
+                    <div>
+                        <label for="writer_name">작성자 이름:</label>
+                        <input id="writer_name"></input>
+                    </div>
+                    <div>
+                        <label for="importance">난이도:</label>
+                        <input id="importance"></input>
+                    </div>
+
+                    <div id="chooga_or_cancel">
+                        <div id="box_add_button">추가</div>
+                        <div id="cancel_button">취소</div>
+                    </div>
+
+                </div>
+            )}
         
         </div>
     )
